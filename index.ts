511b7037623efd57538113f436906c6b4e3d3cd7
samheart564 <samheart564@gmail.com>
2025-10-19 14:30:08 -0700

    saving progress

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import type { ShipBarrageData } from "@/types"
+
+const obj: ShipBarrageData = {
+  ship: [
+    {
+      skillName: "",
+      barrageInfo: {
+        name: "",
+        is_aoa: false,
+        barrageParts: [],
+      },
+    },
+  ]
+}
+
+console.log("Hello via Bun!")
+
---
+++ src/types/barrages.ts
@@ -0,0 +1,86 @@
+// Reusing types from AzurLaneData/types based on research
+export type AimType = number
+export type AmmoType = number
+export type BulletType = number
+export type WeaponType = number
+export type WeaponStat = string
+export type Hull = string
+
+export interface BarrageEntry<T> {
+  type: T
+  buffs?: string[]
+  buff_chance?: number
+}
+
+export interface DamagingBarrage<T> extends BarrageEntry<T> {
+  damage: number
+  count: number
+}
+
+export interface BarrageWeapon extends DamagingBarrage<'weapon'> {
+  fix_damage?: number
+  coefficient: number
+  range: number[]
+  armor_mod: number[]
+  ammo: AmmoType
+  is_critical: boolean
+  shrapnel: boolean
+  ignore_shield: boolean
+  aim_type: AimType
+  splash?: number
+  velocity?: number
+  pierce?: number
+  shell_range?: number[]
+  spread?: number[]
+  reaim?: boolean
+  stat: WeaponStat | 'fleetpower'
+  stat_cap?: number
+  ratio: number
+  angle: number
+  notes: string[]
+  is_air?: boolean
+  airdrop?: boolean
+  tracker?: {
+    angular: number
+    range: number
+  }
+  targeting: string | null
+  centered: boolean
+  weapon_type: WeaponType
+  weapon_id: number
+  bullet_type: BulletType
+  bullet_id: number
+}
+
+export interface BarrageSlash extends DamagingBarrage<'slash'> {
+  level_of: string
+  clears: BulletType[]
+  velocity: number
+  movement_type: 0 | 1 // stationary | travels
+  life_time: number
+  range: number
+  fix_damage: number
+}
+
+export interface BarrageSummon extends BarrageEntry<'summon'> {
+  weapons: Array<BarrageWeapon & { rld: string }>
+  hull: Hull
+  armor: number
+}
+
+export type BarragePart = BarrageWeapon | BarrageSlash | BarrageSummon
+
+export interface ShipBarrageInfo {
+  name: string
+  is_aoa?: boolean
+  barrageParts: BarragePart[]
+}
+
+export interface ShipSkillBarrage {
+  skillName: string
+  barrageInfo: ShipBarrageInfo
+}
+
+export interface ShipBarrageData {
+  ship: ShipSkillBarrage[]
+}
